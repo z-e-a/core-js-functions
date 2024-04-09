73847d0f4d7bc4ca197a3dff2089a1515e0b8888
z-e-a <e.a.zubkov@yandex.ru>
2024-04-09 14:09:15 +0300

    feat: solve task 'getIdGeneratorFunction'

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -219,8 +219,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let curr = startFrom - 1;
+  return () => {
+    curr += 1;
+    return curr;
+  };
 }
 
 module.exports = {
