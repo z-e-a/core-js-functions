0f7a4ab64a25e5f8b75581c127b77c0218456c37
z-e-a <e.a.zubkov@yandex.ru>
2024-04-09 12:28:13 +0300

    feat: solve task 'getFunctionBody'

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -32,8 +32,8 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  return (func ?? '').toString();
 }
 
 /**
