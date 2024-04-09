0eec72ee88ccb4efd06ad52f80ec961025e4e4e3
z-e-a <e.a.zubkov@yandex.ru>
2024-04-09 14:04:31 +0300

    feat: solve task 'logger'

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -176,8 +176,13 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    logFunc(`${func.name}(${JSON.stringify(args).slice(1, -1)}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${JSON.stringify(args).slice(1, -1)}) ends`);
+    return result;
+  };
 }
 
 /**
