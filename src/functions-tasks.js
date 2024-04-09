04acddbe1723830087e06d6a3f3ed3fc8d9758b1
z-e-a <e.a.zubkov@yandex.ru>
2024-04-09 14:05:49 +0300

    feat: solve task 'partialUsingArguments'

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -198,8 +198,8 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args) => fn.call(this, ...args1, ...args);
 }
 
 /**
